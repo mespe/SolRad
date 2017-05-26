a3c90ab614e87b0a9d0bccaa678d488cd56bd412
Matt Espe <mespe@ucdavis.edu>
2017-05-26 11:43:07 -0700

    Adds some comments

--- collection/aod_scrape_sd.R
+++ collection/aod_scrape_sd.R
@@ -6,6 +6,7 @@ library(gdalUtils)
 library(httr)
 #39.464743, -121.734355 are the coordinates of rice research station of interest
 ###user needs to define air_qualDir###
+# Assume that the script is being run in collection/
 dir = "aerosol_rasters"
 if(!file.exists(dir))
     dir.create(dir)
@@ -17,12 +18,14 @@ if (!file.exists(file.path(dir, 'results'))) {
 if (!file.exists(file.path(dir, 'temp'))) {
   dir.create(file.path(dir, 'temp'))
 }
+
 #get coordinates of interest in long lat Clarke 1866 projection (the projection of the MODIS data), assuming coordinates of interest are in long lat WGS84
 crs_clarke <- '+proj=longlat +ellps=clrk66 +no_defs'
 res_station <- SpatialPoints(cbind(-121.734355, 39.464743), proj4string = crs('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
 res_station <- spTransform(res_station, crs_clarke) #coordinates don't change; just the ellipsoid assumption changes
 
 #set up a sequence of dates of interest
+# Do all of this outside of loop to avoid repeated recalculation
 startday <- "055" #this is the first available day of MODIS data
 startyear <- "2000"
 startdate <- strptime(paste0(startday, startyear), '%j%Y')
@@ -36,6 +39,7 @@ yr <- format.Date(datesequence, '%Y')
 base_url <- 'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/6/MOD09CMA/'
 base_url2 <- paste0(base_url, yr, '/', doyr, '/')
 
+# Define once 
 result_file = paste0(dir, '/results/AOD_riceyield_airqual_project.csv')
 
 ## Setup the results file
@@ -43,21 +47,22 @@ if(!file.exists(paste0(dir, '/results/AOD_riceyield_airqual_project.csv')))
     write.csv(data.frame(date=NA,
                          AOT_at_500nm=NA,
                          filename=NA),
-              
-                                        #data_layer=NA,
-                                        #longitude=NA,
-                                        #latitude=NA),
+              # Don't include these, since they will be the same for all
+              # saves disk space and I/O
+              #data_layer=NA,
+              #longitude=NA,
+              #latitude=NA),
               file = result_file,
               row.names = FALSE)
 
-
+# Define the function here and then use inside an mclapply
 foo = function(i){
     x = base_url2[i]
     get_output <- GET(x)
     urls <- readHTMLTable(rawToChar(get_output$content),
                           stringsAsFactors = FALSE)
     fname <- urls$`ftp-directory-list`[3,1]
- #   browser()
+    # Don't download if we already have the data in the results
     if(!any(grepl(fname, readLines(result_file)))){
         file_url <- paste0(x, fname)
         dest_file = paste0(dir, "/temp/", fname)
@@ -81,6 +86,7 @@ foo = function(i){
                                         #data_layer=sbs[12],
                                         #longitude=coordinates(res_station)[1],
                                         #latitude=coordinates(res_station)[2])
+        # Write in append mode to the same file - results might be out of order
         write.table(results, sep = ",",
                     file = result_file,
                     row.names = FALSE, append = TRUE, col.names = FALSE)
@@ -89,10 +95,4 @@ foo = function(i){
 
 mclapply(seq_along(base_url2), function(i) try(foo(i)), mc.cores = 8L)
 
-##     next
-##   }
-##   result <- data.frame(date=datesequence[i], AOT_at_500nm=aod_value, data_layer=sbs[12], filename=fname, longitude=coordinates(res_station)[1], latitude=coordinates(res_station)[2])
-##   ## results <- rbind(results, result) # this is a super inefficient operation
-##   write.csv(results, 'AOD_riceyield_airqual_project.csv', row.names = FALSE)
-## }
 
