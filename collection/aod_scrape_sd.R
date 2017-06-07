1a29c8f16b6924dc1e460ab1e3c9f4d369406438
Matt Espe <mespe@ucdavis.edu>
2017-06-07 10:35:24 -0700

    Adjusts script to lower the number of downloads

--- collection/aod_scrape_sd.R
+++ collection/aod_scrape_sd.R
@@ -1,3 +1,10 @@
+################################################################################
+# WARNING: This script assumes > 20+ cpu cores - do not run on a laptop without
+# adjusting these two parameters:
+
+cores1 = 5L
+cores2 = 4L
+
 #user needs to have GDAL installed on your machine for this work
 #library(RCurl)
 library(XML)
@@ -5,6 +12,8 @@ library(raster)
 library(gdalUtils)
 library(httr)
 library(RCurl)
+library(parallel)
+
 #39.464743, -121.734355 are the coordinates of rice research station of interest
 ###user needs to define air_qualDir###
 # Assume that the script is being run in collection/
@@ -33,10 +42,9 @@ startdate <- strptime(paste0(startday, startyear), '%j%Y')
 enddate <- strptime("12/31/2016", '%m/%d/%Y')
 datesequence <- seq.Date(from=as.Date(startdate), to=as.Date(enddate), by='day')
 
-library(parallel)
-
 doyr <- format.Date(datesequence, '%j')
 yr <- format.Date(datesequence, '%Y')
+
 # base_url <- 'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/6/MOD09CMA/'
 # Look at this different data product
 base_url <- 'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/6/MOD04_L2/'
@@ -51,18 +59,17 @@ if(!file.exists(paste0(dir, '/results/AOD_riceyield_airqual_project.csv')))
                          Corrected_Optical_Depth_Land = NA,
                          Corrected_Optical_Depth_Land_wav2p1 = NA,
                          filename=NA),
-              # Don't include these, since they will be the same for all
-              # saves disk space and I/O
-              #data_layer=NA,
-              #longitude=NA,
-              #latitude=NA),
               file = result_file,
               row.names = FALSE)
+
 # Data we are interested in from other data products:
 # corrected optical depth land
 # aerosol type land
-# 
-curl = getCurlHandle(useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36", followlocation = TRUE, verbose = TRUE)
+# https://modis-atmos.gsfc.nasa.gov/MOD04_L2/format.html
+
+# Set curl handle so repeated connections use the same handle
+curl = getCurlHandle(useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                     followlocation = TRUE, verbose = TRUE)
 
 get_target_links = function(base_urls, curl = getCurlHandle())
 {
@@ -74,24 +81,31 @@ get_target_links = function(base_urls, curl = getCurlHandle())
     do.call(c, links)
 }
 
+# Subset to only the flights where the sensors are over our area of interest
 tars = get_target_links(base_url2)
-
+times = as.numeric(gsub(".*MOD04_L2\\.A[0-9]+\\.([0-9]{4})\\..*", "\\1", tars))
+tars = tars[times > 1700 & times < 2100]
 
 # Define the function here and then use inside an mclapply
-get_MODIS_subset= function(tar){
+get_MODIS_subset= function(tar, cores2 = cores2){
 
     fname = gsub(".*(MOD04_L2[.]A.*$)", "\\1", tar)
+
     # Don't download if we already have the data in the results
     if(!any(grepl(fname, readLines(result_file)))){
         file_url <- paste0("https://ladsweb.modaps.eosdis.nasa.gov", tar)
         dest_file = paste0(dir, "/temp/", fname)
-        ## setwd(file.path(dir, 'temp')) # This is dangerous
+        # this fixed a problem
+        # https://gis.stackexchange.com/questions/213923/open-hdf4-files-using-gdal-on-windows
         download.file(file_url, destfile = dest_file,
-                      mode = 'wb') #this fixed a problem https://gis.stackexchange.com/questions/213923/open-hdf4-files-using-gdal-on-windows
+                      mode = 'wb') 
         fname_tif <- gsub('hdf', 'tif', dest_file, fixed=TRUE) 
-        ##gdalinfo(fname) #we got a good ole hdf4 file!  must set mode='wb' in download.file above for this to work or manually download files from the URL on my windows machine
-        sbs <- get_subdatasets(dest_file) #we need subdataset 12 "Coarse Resolution AOT at 550 nm"
-        sbs_sub_idx = grep("Aerosol_Type_Land|Corrected_Optical_Depth_Land|Corrected_Optical_Depth_Land_wav2p1",
+        sbs <- get_subdatasets(dest_file)
+        regstring = paste(c("Aerosol_Type_Land",
+                            "Aerosol_Cloud_Fraction_Land",
+                            "Corrected_Optical_Depth_Land",
+                            "Corrected_Optical_Depth_Land_wav2p")
+        sbs_sub_idx = grep(,
                            sbs)
         ans = do.call(c, mclapply(sbs_sub_idx, function(i){
             fname_tmp = gsub(".tif", paste0(i,".tif"), fname_tif)
@@ -105,7 +119,7 @@ get_MODIS_subset= function(tar){
             aod_value <- extract(aod_tif, res_station)
             structure(aod_value,
                       names = gsub("^.*\\.hdf:mod04:", "", sbs[i]))
-        }, mc.cores = 3L))
+        }, mc.cores = cores2))
         
         file.remove(dest_file)
         
@@ -117,6 +131,6 @@ get_MODIS_subset= function(tar){
     }
 }
 
-mclapply(tars, function(x) try(get_MODIS_subset(x)), mc.cores = 5L)
+mclapply(tars, function(x) try(get_MODIS_subset(x)), mc.cores = cores1)
 
 
